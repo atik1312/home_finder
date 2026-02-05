37a7b53db548ba536f9e6eeb28fa400c80505964
atik1312 <shariaratik95@gmail.com>
2026-02-05 15:11:24 +0600

    log in page update

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -27,55 +27,117 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child:ListView(
-            padding: const EdgeInsets.all(24.0),
-            shrinkWrap: true,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: TextFormField(
-                  controller: _emailController,
-                  keyboardType:TextInputType.emailAddress ,
-                  decoration: InputDecoration(
-                    filled: true,
-                    prefixIcon: const Icon(Icons.email),
-                    labelText: "Email Address"
-                  ),
-                  validator: (value) {
-                    if(value==null|| value.isEmpty){
-                      return 'Provide a valid email address';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: TextFormField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    prefixIcon: const Icon(Icons.password),
-                    labelText: "Password (atleast 6 characters)"
-                  ),
-                  validator: (value) {
-                    if(value==null|| value.isEmpty||value.length<6){
-                      return 'Provide a valid password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              ElevatedButton(onPressed: _authenticate
-              , child: Text("Login as Admin")),
-              Text(_errMsg,style: TextStyle(fontSize: 18,color: Colors.red),)
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [const Color.fromARGB(255, 224, 211, 248),const Color.fromARGB(255, 236, 234, 243)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
           )
+        ),
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16.0),
+            margin: const EdgeInsets.all(24.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.0)
+            ),
+            child: Form(
+              key: _formKey,
+              child:ListView(
+                padding: const EdgeInsets.all(10.0),
+                shrinkWrap: true,
+                children: [
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  //   children:  [
+                  //     Container(
+                  //       padding: const EdgeInsets.all(10),
+                  //       decoration: BoxDecoration(
+                  //         color: const Color.fromARGB(255, 235, 245, 192),
+                  //         borderRadius: BorderRadius.circular(8.0)
+                  //       ),
+                  //       child: Text("Admin ",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold)),
+                  //     ),
+                  //     Container(
+                  //       padding: const EdgeInsets.all(10),      
+                  //       decoration: BoxDecoration(
+                  //         color: const Color.fromARGB(255, 235, 245, 192),
+                  //         borderRadius: BorderRadius.circular(8.0)
+                  //       ),               
+                  //       child: Text("User ",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold)),
+                  //     ),
+                  //   ],
+                  // ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: TextFormField(
+                      controller: _emailController,
+                      keyboardType:TextInputType.emailAddress ,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.email),
+                        hintText: "Email Address",
+                        filled: true,
+                        fillColor: const Color.fromARGB(255, 235, 245, 192),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(8.0)
+                        )
+                      ),
+                      validator: (value) {
+                        if(value==null|| value.isEmpty){
+                          return 'Provide a valid email address';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 8.0,),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: TextFormField(
+                      obscureText: true,
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        prefixIcon: const Icon(Icons.password),
+                        hintText: "Password (atleast 6 characters)",
+                         fillColor: const Color.fromARGB(255, 235, 245, 192),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(8.0)
+                        )
+                      ),
+                      validator: (value) {
+                        if(value==null|| value.isEmpty||value.length<6){
+                          return 'Provide a valid password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 8.0,),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 100),
+                  child: ElevatedButton(onPressed: _authenticate,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 243, 247, 198),
+                      foregroundColor: Colors.black,  
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0)
+                      )
+                    ),
+                    child: Text("Login")),
+                  ),
+                  SizedBox(height: 8.0,),
+                  _errMsg.isNotEmpty? Text(_errMsg,style: TextStyle(fontSize: 18,color: Colors.red),):SizedBox.shrink()
+                ],
+              )
+              ),
           ),
+        ),
       ),
     );
   }
